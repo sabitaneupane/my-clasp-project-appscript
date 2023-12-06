e3d8f923150c1eb982dcfeb8584d3e9934db0cfe
Sabita Neupane <nsabita.sasa@gmail.com>
2023-12-06 12:54:44 +0545

    update folder str

--- Code.js
+++ Code.js
@@ -2,7 +2,7 @@ const senderInfo = {
   name: "YOUR_NAME",
 };
 
-const sheetId = "YOUR_SHEET_ID";
+const sheetId = "";
 const myHeaders = {
   name: "Name",
   dob: "DOB",
@@ -13,6 +13,6 @@ const headerIndex = 0; // Assuming the first row contains headers
 
 let currentDate = new Date();
 
-function main (){
+function processAction (){
     readUserInfo();
 }
